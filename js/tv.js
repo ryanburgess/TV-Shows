87fda59e9811e4e36e41307817477d2c99774b88
Ryan <rburgess@evernote.com>
2014-02-16 12:26:34 -0800

    change tv shows to a JS array instead of html list

--- js/tv.js
+++ js/tv.js
@@ -1,14 +1,122 @@
 (function($) {
     "use strict";
+
+    var shows = ["2 Broke Girls",
+			"30 Rock",
+			"666 Park Avenue",
+			"90210",
+			"Adventure Time",
+			"Alcatraz",
+			"American Horror Story",
+			"Anger Management",
+			"Angry Boys",
+			"Archer",
+			"Arrested Development",
+			"Bad Girls Club",
+			"Best Ink",
+			"Better off Ted",
+			"Big Brother",
+			"Boardwalk Empire",
+			"Bob's Burgers",
+			"Bored to Death",
+			"Breaking Bad",
+			"Brotherhood",
+			"Burn Notice",
+			"Californication",
+			"Community",
+			"Covert Affairs",
+			"Criminal Minds",
+			"Desperate Housewives",
+			"Dexter",
+			"Eastbound & Down",
+			"Elementary",
+			"Entourage",
+			"Episodes",
+			"Family Guy",
+			"Flight of the Conchords",
+			"Futurama",
+			"Game of Thrones",
+			"Girls",
+			"Gossip Girl",
+			"Happy Endings",
+			"Homeland",
+			"Hostages",
+			"House of Cards",
+			"House of Lies",
+			"How I Met Your Mother",
+			"How to Make it In America",
+			"Hung",
+			"Ink Master",
+			"It's Always Sunny In Philadelphia",
+			"Ja'mie: Private School Girl",
+			"Jersey Shore",
+			"LA Ink",
+			"Legit",
+			"Lie to Me",
+			"Lost",
+			"Mad Men",
+			"Married With Children",
+			"Modern Family",
+			"Mom",
+			"Napoleon Dynamite",
+			"New Girl",
+			"Nurse Jackie",
+			"Orange Is The New Black",
+			"Parenthood",
+			"Parks And Recreation",
+			"Partners",
+			"Party Down",
+			"Person of Interest",
+			"Portlandia",
+			"Prime Suspect",
+			"Project Runway",
+			"Revenge",
+			"Ringer",
+			"Rules of Engagement",
+			"Scandal",
+			"Seinfeld",
+			"Shameless",
+			"Shark Tank",
+			"Six Feet Under",
+			"Sons of Anarchy",
+			"South Park",
+			"Suburgatory",
+			"Suits",
+			"Summer Heights High",
+			"Tattoo Nightmares",
+			"The Awesomes",
+			"The Bad Girl's Club",
+			"The Big Bang Theory",
+			"The Blacklist",
+			"The Crazy Ones",
+			"The Following",
+			"The Good Wife",
+			"The IT Crowd",
+			"The L Word",
+			"The Mentalist",
+			"The Mindy Project",
+			"The Office",
+			"The Playboy Club",
+			"The Real Housewives of Vancouver",
+			"The Walking Dead",
+			"The Wire",
+			"Transporter",
+			"Trophy Wife",
+			"True Blood",
+			"Two And A Half Men",
+			"Up All Night",
+			"Vegas",
+			"We Are Men",
+			"Weeds",
+			"Workaholics"];
     
     function tvData(title){
 	    $.ajax({
-	      url: "http://www.imdbapi.com/?t=" + $(title).text(),
+	      url: "http://www.imdbapi.com/?t=" + title,
 	        dataType: 'json',
 	        success: function(data){
 	            var year = data.Year;
 	            var image = data.Poster;
-	            var show = $(title).text();
 	            var rating = data.imdbRating;
 	            var genre = data.Genre;
 	            var description = data.Plot;
@@ -17,9 +125,13 @@
 	            	image = "default.jpg";
 	            }
 	            
-	            var html = "<img src='"+ image +"' alt='"+ show +" poster'>";
+	            var genreClass = genre.toLowerCase();
+	            genreClass = genreClass.replace(/,/g, "");
+	            var html = "";
+	            html += "<li class='show-item "+ genreClass +"'>";
+	            html += "<img src='"+ image +"' alt='"+ title +" poster'>";
 	            html += "<div class='content'>";
-	            html += "<h2>"+ show +"</h2>";
+	            html += "<h2>"+ title +"</h2>";
 	            html += "<ul class='info'>";
 	            html += "<li class='date'>("+ year +")</li>";
 	            html += "<li class='rating'>Rating: "+ rating +"</li>";
@@ -27,24 +139,20 @@
 	            html += "</ul>";
 	            html += "<p>"+ description +"</p>"
 	            html += "</div>";
-	            $(title).html(html);
+	            html += "</li>";
 
-	            $(title).addClass("show-item");
-
-	            var genreClass = genre.toLowerCase();
-	            genreClass = genreClass.replace(/,/g, "");
-	            $(title).addClass(genreClass);
+	            $(".shows").append(html);
 	        }
 	    });
 	            
 	}
 
-	$(".shows li").each(function() {
-	      tvData(this);
-	});
+	for (var i = 0; i < shows.length; i++){
+	    tvData(shows[i]);
+	}
 
 
-	$(".genres a").on("click", function(){
+	$(".genres").on("click", "a", function(){
 		var genreClick = $(this).attr("href");
 		genreClick = genreClick.replace("#", "");
 
@@ -60,5 +168,4 @@
 		
 	});
 	
-    
 }(jQuery));
